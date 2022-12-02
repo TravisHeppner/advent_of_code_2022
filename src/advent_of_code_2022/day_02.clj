f563d883e18204556e2f5aa8bec36e74c43423f5
Travis Heppner <travis.roth.heppner@gmail.com>
2022-12-02 09:15:52 -0700

    day 2 solution

--- src/advent_of_code_2022/day_02.clj
+++ src/advent_of_code_2022/day_02.clj
@@ -5,6 +5,51 @@
 
 (def input (utils/read-prompt "day_02.txt"))
 
+(def translate
+  {"A" 1 "X" 1 "Y" 2 "B" 2 "Z" 3 "C" 3})
+
 (defn parse-input [input-string]
-  (->> (cs/split input-string #"(\r?\n){2}")
-       (map #(map read-string (cs/split-lines %)))))
+  (->> input-string
+       cs/split-lines
+       (map #(map translate (cs/split % #" ")))))
+
+
+(defn score [[them me]]
+  (+ me
+     (cond
+       (= them  me) 3
+       (= (mod them 3) (mod (dec me) 3)) 6
+       :default 0)))
+(let [sample (parse-input "A Y\nB X\nC Z")
+      matches (map score sample)
+      total (reduce + matches)]
+  (println "sample score: " matches " -> "total))
+
+(def part1
+  (->> input
+       parse-input
+       (map score)
+       (reduce +)))
+(println "part1 ->" part1)
+
+(defn match-res [[them result]]
+  [them
+   (case result
+     1 (inc (mod (dec (dec them)) 3))
+     2 them
+     3 (inc (mod them 3)))])
+
+
+(let [sample (parse-input "A Y\nB X\nC Z")
+      matches (map (comp score match-res) sample)
+      total (reduce + matches)]
+  (println "sample score: " matches " -> "total))
+
+
+(def part2
+  (->> input
+       parse-input
+       (map (comp score match-res))
+       (reduce +)))
+(println "part2 ->" part2)
+
